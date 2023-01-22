4f1769bc9f97c145952a1e213af76b6909b6891f
Sparkles <smcgarr13@yahoo.com>
2023-01-21 22:49:46 -0600

    updated-js

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,5 +1,5 @@
-var APIKey = "3be84550c5msh6225c2c3674dcaep112662jsn51bef5314860";
-
+// var APIKey = "3be84550c5msh6225c2c3674dcaep112662jsn51bef5314860";
+var APIKey = "f81d281d6amsh310843a7957d922p1d1b9cjsn8e63eaaea121";
 const settings = {
   async: true,
   crossDomain: true,
@@ -32,7 +32,7 @@ $(document).ready(function () {
     //   playlistName: playlistName.value,
     // };
     // playlistNameInput.push(data)
-var playlistName = $("#playlist-name");
+    var playlistName = $("#playlist-name");
     localStorage.setItem("createPlaylist", "playlistName");
     localStorage.getItem("playlistName" == "playlistNameInput");
     // playlistName.value = localStorage.getItem("playlistName");
@@ -62,7 +62,7 @@ function getMusic(event) {
       APIKey,
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "3be84550c5msh6225c2c3674dcaep112662jsn51bef5314860",
+      "X-RapidAPI-Key": "f81d281d6amsh310843a7957d922p1d1b9cjsn8e63eaaea121",
       "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
     },
   };
@@ -70,5 +70,97 @@ function getMusic(event) {
   $.ajax(settings).done(function (response) {
     console.log(response);
     //for loop, for loop is going to run for the length/duration, then create an HTML card or element that includes an add button, in the for loop we'll attach event listeners, then append it to the HTML, then work on the function for the add button
+
+    //Audio Player
+    // $('')
+
+    //   });
+    // }
   });
+}
+
+const settings2 = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://deezerdevs-deezer.p.rapidapi.com/radio/KIIS%20fm",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "f81d281d6amsh310843a7957d922p1d1b9cjsn8e63eaaea121",
+		"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+
+$(document).ready(function () {
+  $("ul.list.li").click(function () {
+    $("ul.list.li").removeClass("active");
+    $(this).addClass("active");
+
+    let trackID = $(this).attr("track-id");
+    $(".single .track").removeClass("active");
+    $(trackId).addClass("active");
+  });
+
+  let audioURL = "https://api.deezer.com/album/8938827/tracks";
+  
+//   "https://deezerdevs-deezer.p.rapidapi.com/radio/%7Bid%7D";
+
+  let audio = {};
+  audio["track"] = new Audio();
+  audio["track"].src = audioURL;
+
+  audio["track"].addEventListener("canplaythrough", function () {
+    $(".time").html(
+      new Date(Math.round(audio["music"].duration) * 1000)
+        .toISOString()
+        .substr(11, 8)
+    );
+  });
+
+  let interval;
+  let pIndex = 0;
+  $(".play").click(function () {
+    console.log("Button Works!!");
+
+    let audioDuration = Math.round(audio["track"].duration);
+    let itemTimeout = audioDuration;
+
+    if (isPlaying(audio["track"])) {
+      audio["track"].pause();
+      $(".play svg").removeClass("fa-pause").addClass("fa-play");
+
+      clearInterval(interval);
+    } else {
+      audio["track"].play();
+      $("play svg").removeClass("fa-play").addClass("fa-pause");
+
+      interval = setInterval(() => {
+        pIndex++;
+        if (audio["track"].currentTime >= audioDuration) {
+          clearInterval(interval);
+          audio["track"].pause();
+          pIndex = 1;
+          audio["track"].currentTime = 0;
+          $(".play svg").removeClass("fa-pause").addClass("fa-play");
+        }
+        $(".time").html(
+          new Date(Math.round(audio["track"].currentTime) * 1000)
+            .toISOString()
+            .substr(11, 8)
+        );
+
+        $(".seekbar-inner").css({ width: pIndex / 10 + "%" });
+      }, itemInterval);
+    }
+  }, 200);
+});
+
+// });
+
+function isPlaying(audElem) {
+  return !audElem.paused;
 }
