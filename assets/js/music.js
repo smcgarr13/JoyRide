f999f020e39ffebe3c5f38ddb667b4e393df8316
Sparkles <smcgarr13@yahoo.com>
2023-01-24 20:00:30 -0600

    audio player dynamic

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,7 +1,7 @@
 var APIKey = "3be84550c5msh6225c2c3674dcaep112662jsn51bef5314860";
 // var APIKey = "f81d281d6amsh310843a7957d922p1d1b9cjsn8e63eaaea121";
 
-var playlist = ["How I Got Over", "Sometimes I Rhyme Slow", "On A Sunday Afternoon", "ATLiens", "Can I Kick It?", "Love (Hip Hop)", "To Live & Die in LA", "Summertime In the LBC", "Hate It Or Love It","Feel Me Flow"];
+var playlist = ["How I Got Over", "Sometimes I Rhyme Slow", "On A Sunday Afternoon", "ATLiens", "Can I Kick It?", "Summertime In the LBC", "Hate It Or Love It","Feel Me Flow", "Shimmy Shimmy Ya", "Still Not a Player", "What You Know", "Overnight Celebrity", "Black and Yellow", "Young, Wild & Free"];
 
 
 // I want to play my playlist using Deezer API
@@ -20,70 +20,22 @@ var playlist = ["How I Got Over", "Sometimes I Rhyme Slow", "On A Sunday Afterno
 
 
 $(document).ready(function () {
-  // add listner for click event
-  var createButton = $("#create-button");
-  createButton.on("click", createPlaylist);
-
-  // save playlist name to local storage
-  var createPlaylist = $("#create-playlist");
-  function createPlaylist(event) {
-    var playlistNameInput = document.querySelector("#playlist-name-input");
-    event.preventDefault();
-    console.log("Button Works!!");
-    console.log(playlistNameInput);
-
-    // var data = {
-    //   playlistName: playlistName.value,
-    // };
-    // playlistNameInput.push(data)
-    var playlistName = $("#playlist-name");
-    localStorage.setItem("createPlaylist", "playlistName");
-    localStorage.getItem("playlistName" == "playlistNameInput");
-    // playlistName.value = localStorage.getItem("playlistName");
-  }
-});
-
-// add listner for click event
-var searchButton = $("#search-button");
-searchButton.on("click", getMusic);
-
-function getMusic(event) {
-  event.preventDefault();
-  console.log("Button Works!!");
-
-  // save to local storage
-  var playlistArtist = $("#playlist-artist").val();
-  // get user input that was saved in localStorage
-  // [=]
-
-  const settings = {
-    async: true,
-    crossDomain: true,
-    url:
-      "https://deezerdevs-deezer.p.rapidapi.com/search?q=" +
-      playlistArtist +
-      "&appid=" +
-      APIKey,
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "f81d281d6amsh310843a7957d922p1d1b9cjsn8e63eaaea121",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-    },
-  };
-
-  $.ajax(settings).done(function (response) {
-    console.log(response);
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url:
+          "https://deezerdevs-deezer.p.rapidapi.com/search?q=",
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": "f81d281d6amsh310843a7957d922p1d1b9cjsn8e63eaaea121",
+          "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+        },
+      };
     
-    // var currentTrack = document.querySelector("#current-track");
+      $.ajax(settings).done(function (response) {
+        console.log(response);
 
-    // currentTrack.src=response.data[0].preview
-
-
-
-
-
-
-// add listner for click event
+        // add listner for click event
 // $(document).ready(function () {
     $("ul.list.li").click(function() {
         $("ul.list.li").removeClass("active");
@@ -101,7 +53,6 @@ function getMusic(event) {
         $(".time").html(new Date(Math.round(audio["track"].duration) * 1000).toISOString().substr(11, 8));
       });
     
-
 
       let interval;
       let pIndex = 0;
@@ -140,52 +91,76 @@ function getMusic(event) {
 
 
           let audioURL = response.data[0].preview;
-      
+
           //   "https://deezerdevs-deezer.p.rapidapi.com/radio/%7Bid%7D";
           
             
-            audio["track"].src = audioURL;
+          audio["track"].src = audioURL;
 
-            setTimeout(function() {
-    
-                let audioDuration = Math.round(audio["track"].duration);
-                let itemInterval = audioDuration;
-            
-                if (isPlaying(audio["track"])) {
-                  audio["track"].pause();
-                  $(".play svg").removeClass("fa-pause").addClass("fa-play");
-            
-                  clearInterval(interval);
-                } else {
-                  audio["track"].play();
-                  $(".play svg").removeClass("fa-play").addClass("fa-pause");
-            
-                  interval = setInterval(() => {
-                    pIndex++;
-                    if (audio["track"].currentTime >= audioDuration) {
-                      clearInterval(interval);
-                      audio["track"].pause();
-                      pIndex = 1;
-                      audio["track"].currentTime = 0;
-                      $(".play svg").removeClass("fa-pause").addClass("fa-play");
-                    }
-                    $(".time").html(new Date(Math.round(audio["track"].currentTime) * 1000).toISOString().substr(11, 8));
-            
-                    $(".seekbar-inner").css({ width: pIndex / 10 + "%" });
-                  }, itemInterval);
-                }
-              }, 200);
+          setTimeout(function() {
+  
+              let audioDuration = Math.round(audio["track"].duration);
+              let itemInterval = audioDuration;
+          
+              if (isPlaying(audio["track"])) {
+                audio["track"].pause();
+                $(".play svg").removeClass("fa-pause").addClass("fa-play");
+          
+                clearInterval(interval);
+              } else {
+                audio["track"].play();
+                $(".play svg").removeClass("fa-play").addClass("fa-pause");
+          
+                interval = setInterval(() => {
+                  pIndex++;
+                  if (audio["track"].currentTime >= audioDuration) {
+                    clearInterval(interval);
+                    audio["track"].pause();
+                    pIndex = 1;
+                    audio["track"].currentTime = 0;
+                    $(".play svg").removeClass("fa-pause").addClass("fa-play");
+                  }
+                  $(".time").html(new Date(Math.round(audio["track"].currentTime) * 1000).toISOString().substr(11, 8));
+          
+                  $(".seekbar-inner").css({ width: pIndex / 10 + "%" });
+                }, itemInterval);
+              }
+            }, 200);
 
-        });
+      });
+  });
+  });
+  
+  function isPlaying(audElem) {
+      return !audElem.paused;
+    };
+
+
+});
+
+// want audio player to automatically play the next song
+
+// once current song ends, the next song will begin to play
+// need function to play next song automatically
+// would we need a for loop to achieve this?
+// 
 
 
 
-       
-    });
-    });
-    
-    function isPlaying(audElem) {
-        return !audElem.paused;
-      };
-    
-};
+// want the current play button to start the playlist and pause the song when pressed again
+
+// create if/else statement to change the play function to pause
+// create if/else statement to change the pause function back to play
+
+
+
+//want status bar to reset when a new song starts
+
+// clear interval when current song ends?
+
+
+
+
+// want the songs in queue to show in the smaller windows
+
+// 
